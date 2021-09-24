6f235bd2e4b9bda9293f8de9cc88e3553fd98874
SofianD <doual.sofian@hotmail.com>
2021-09-24 17:38:32 +0200

    Test 06

--- test/06.test.js
+++ test/06.test.js
@@ -2,21 +2,21 @@ const { test, expect } = require('@jest/globals');
 const prox = require('../06')
 describe('Proxy', () => {
     test(
-        'get with good param returns value',
+        'called with good param returns value',
         () => {
             expect(prox.foo).toBe(10**2)
         }
     )
 
     test(
-        'get with bad param throw an error',
+        'called with bad param throw an error',
         () => {
             expect(() => {return prox.bar}).toThrow('Unknown property bar in target object!')
         }
     )
     
     test(
-        'set with good aruments',
+        'set param with good aruments',
         () => {
             prox.bar = 5
             expect(prox.bar).toBe(5**2)
@@ -24,7 +24,7 @@ describe('Proxy', () => {
     )
 
     test(
-        'set with bad aruments',
+        'set param with bad aruments',
         () => {
             expect(() => {
                 return prox.bar = 'foo'
