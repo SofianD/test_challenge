7a47ad53907d1c41e75f7918232907595e9e098c
SofianD <doual.sofian@hotmail.com>
2021-09-27 17:20:03 +0200

    Test 07

--- test/07.test.js
+++ test/07.test.js
@@ -2,6 +2,23 @@ const { test, expect } = require('@jest/globals');
 
 const foo = require('../07')
 
-// describe('Generator', () => {
-//     test()
-// })
+describe('Foo', () => {
+    test(
+        'works',
+        () => {
+            const arr = [1, NaN, 5n, 0n]
+            const gen = foo()
+            arr.forEach((value) => {
+                expect(gen.next().value).toEqual(value);
+            })
+        }
+    )
+
+    test(
+        'works with string',
+        () => {
+            const gen = foo('foo')
+            expect(gen.next().value).toEqual('1foo');
+        }
+    )
+})
